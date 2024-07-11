a82df86dd51c5aee72066a06cae8b4220719854a
onlineresultsite <footflux@gmail.com>
2024-07-11 17:21:21 +0530

    all files new upload

---
+++ scripts/python_dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Create a virtual environment
+python3 -m venv /home/ubuntu/env
+
+# Activate the virtual environment
+source /home/ubuntu/env/bin/activate
+
+# Upgrade pip, setuptools, and wheel
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
