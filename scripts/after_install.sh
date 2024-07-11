4bd549618b64a2e1b06a8c0eb25b1522d979e4a8
onlineresultsite <footflux@gmail.com>
2024-07-11 16:06:20 +0530

    after install file add

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,24 @@
-#!/usr/bin/bash
-
 echo "Pull Finished"
+
+# Test Nginx configuration before reloading
+sudo nginx -t
+if [ $? -ne 0 ]; then
+  echo "Nginx configuration error. Aborting."
+  exit 1
+fi
+
+# Reload systemd manager configuration
 sudo systemctl daemon-reload
+if [ $? -ne 0 ]; then
+  echo "Systemctl daemon-reload failed. Aborting."
+  exit 1
+fi
+
+# Restart Nginx
 sudo systemctl restart nginx
+if [ $? -ne 0 ]; then
+  echo "Failed to restart Nginx. Aborting."
+  exit 1
+fi
+
+echo "Deployment successful"
