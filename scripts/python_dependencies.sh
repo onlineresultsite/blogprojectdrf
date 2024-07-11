ddf9593f28bce4f57dc9150675a6c3628842d88d
onlineresultsite <footflux@gmail.com>
2024-07-11 17:28:24 +0530

    conf file update

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# Create a virtual environment
-python3 -m venv /home/ubuntu/env
-
-# Activate the virtual environment
+virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
-
-# Upgrade pip, setuptools, and wheel
-pip install --upgrade pip setuptools wheel
-
-# Install dependencies
 pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
