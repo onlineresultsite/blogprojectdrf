c613574cc43f97097f6fc5ac7bcfff604e1b3c88
onlineresultsite <footflux@gmail.com>
2024-07-16 01:03:23 +0530

    tests

---
+++ scripts/trigger_github_workflow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GITHUB_TOKEN="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"
+REPO_OWNER="onlineresultsite"
+REPO_NAME="blogprojectdrf"
+API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/dispatches"
+
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
+     -H "Accept: application/vnd.github.v3+json" \
+     --data '{"event_type": "run-pytest"}' \
+     $API_URL
+
+
+# chmod +x scripts/trigger_github_workflow.sh      run manual
