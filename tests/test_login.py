fb63c36d8ab6c05c5197acf7740048436348a675
onlineresultsite <footflux@gmail.com>
2024-07-15 18:22:19 +0530

    add tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,23 +11,11 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill
 
 # Initialize the workbook and sheet outside the functions
-wb = Workbook()
-sheet = wb.active
-sheet.title = "Test Results"
 
-# Add header row to the sheet
-headers = ["Username", "Password", "Status", "Message"]
-sheet.append(headers)
 
-# Define a style for the header row
-header_font = Font(bold=True)
-header_fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
 
-for cell in sheet[1]:
-    cell.font = header_font
-    cell.fill = header_fill
 
-row_counter = 2
+
 
 @pytest.fixture(scope="module")
 def setup():
@@ -81,8 +69,7 @@ def login(driver, username, password):
     finally:
         # Write results to the Excel sheet
         global row_counter
-        sheet.append([username, password, status, message])
-        row_counter += 1
+
 
 @pytest.mark.parametrize("username, password", [
     ("akshay", "akTR@300"),
@@ -91,6 +78,5 @@ def test_my_requests(setup, username, password):
     driver = setup
     login(driver, username, password)
 
-# Save the results to an Excel file after all tests have been run
-def pytest_sessionfinish(session, exitstatus):
-    wb.save("test_results.xlsx")
+
+
