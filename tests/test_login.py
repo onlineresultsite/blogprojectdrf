992d6891407adbf82cc5e36731ac30142b9043a2
onlineresultsite <footflux@gmail.com>
2024-07-16 17:54:46 +0530

    dgfd

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,6 +7,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+import pytest
+import openpyxl
+from openpyxl.styles import Font, PatternFill
+from openpyxl import Workbook
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
 
 # Define the URL, username, and password
 URL = "http://13.53.206.233:8000/login/"  # Replace with your login URL
@@ -14,7 +26,7 @@ USERNAME = "akshay"
 PASSWORD = "akTR@300"
 
 @pytest.fixture(scope="module")
-def browser():
+def steup():
     options = Options()
     options.add_argument("--headless")  # Run in headless mode
     options.add_argument("--disable-gpu")
@@ -22,24 +34,26 @@ def browser():
     yield driver
     driver.quit()
 
-def test_login(browser):
-    browser.get(URL)
+def test_login(driver):
+    driver.get(URL)
     
     # Find the username and password input fields and the login button
-    username_field = browser.find_element(By.ID, "username")  # Adjust the locator as needed
-    password_field = browser.find_element(By.ID, "form2Example22")  # Adjust the locator as needed
-    login_button = browser.find_element(By.XPATH, "/html/body/div/section/div/div/div/div/div/div[1]/div/form/div[3]/button")  # Adjust the locator as needed
+    username_input = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH, "//input[@name='username']"))
+        )
+    username_input.send_keys(USERNAME)
+    password_input = driver.find_element(By.XPATH, "//input[@name='password']")
+    password_input.send_keys(PASSWORD)
 
-    # Input the username and password
-    username_field.send_keys(USERNAME)
-    password_field.send_keys(PASSWORD)
-    
-    # Click the login button
+        # Click on the login button
+    login_button = driver.find_element(By.XPATH, "//button[text()='Log In']")
     login_button.click()
+
+
 
     # Verify login success by checking for a specific element that is present only on the login success page
     try:
-        success_element = browser.find_element(By.ID, "/html/body/div/header/nav/div/div/a[1]/span")  # Adjust the locator as needed
+        success_element = driver.find_element(By.ID, "/html/body/div/header/nav/div/div/a[1]/span")  # Adjust the locator as needed
         assert success_element.is_displayed()
         print("Login successful")
         with open("report.html", "w") as report:
