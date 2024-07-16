2a1832fc9ea93f66a54bb974ebb6777aa0bfa6c3
onlineresultsite <footflux@gmail.com>
2024-07-16 11:28:00 +0530

    sdf

--- scripts/trigger_github_workflow.sh
+++ scripts/trigger_github_workflow.sh
@@ -3,7 +3,10 @@
 GITHUB_TOKEN="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"
 REPO_OWNER="onlineresultsite"
 REPO_NAME="blogprojectdrf"
-API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/dispatches"
+API_URL="https://api.github.com/repos/onlineresultsite/blogprojectdrf"
+
+
+
 
 curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
      -H "Accept: application/vnd.github.v3+json" \
