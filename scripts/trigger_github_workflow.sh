60857b94390de48b8a3dde0f5f4356ae0566ae44
onlineresultsite <footflux@gmail.com>
2024-07-16 11:49:28 +0530

    sdf

--- scripts/trigger_github_workflow.sh
+++ scripts/trigger_github_workflow.sh
@@ -3,8 +3,8 @@
 GITHUB_TOKEN="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"
 REPO_OWNER="onlineresultsite"
 REPO_NAME="blogprojectdrf"
-API_URL="https://api.github.com/repos/onlineresultsite/blogprojectdrf"
-
+API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/dispatches"
+# https://api.github.com/repos/onlineresultsite/blogprojectdrf
 
 
 
